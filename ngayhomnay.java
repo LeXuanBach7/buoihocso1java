dc1ce49f92e725f0851e37b49b6138b1328a6806
LeXuanBach7 <ble417265@gmail.com>
2025-04-10 10:15:34 +0700

    test chuong trinh cua toi

---
+++ ngayhomnay.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ngayhomnay {
+
+    public static void main(String[] args) {
+        String ten = "lê xuân bách";
+        int tuoi = 30;
+        double chieucao= 200;
+        System.out.println("Tên:"+ten);
+        System.out.println("name:"+tuoi);
+        System.out.println("chieucao:"+chieucao+"cm");
+    }
+}
